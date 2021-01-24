53886d6add634fa6a279c8deca014d00bdd28593
Salman Zahid <salmanzahid2982@gmail.com>
2021-01-24 17:16:43 +0530

    working on useEffect

--- Frontend/src/typoComponents/Typo.js
+++ Frontend/src/typoComponents/Typo.js
@@ -1,11 +1,13 @@
-import React,{ useState,useRef } from 'react'
+import React,{ useState,useRef,useEffect } from 'react'
 
 import Base from '../core/Base.js'
 import Preview from './Preview.js'
 import Speed from './Speed.js'
 
 const Typo=()=> {
-    let interval = useRef(null)
+    //const interval = useRef(null);
+
+    const [isActive, setIsActive] = useState(false);
     const [values, setValues] = useState
     (
         {text:"Test",
@@ -17,6 +19,24 @@ const Typo=()=> {
         finished:false
         }
     )
+
+    useEffect(() => {
+        const interval = null;
+        if (isActive) {
+            interval = setInterval(() => {
+                setValues(prevProps => (
+                {
+                    ...prevProps,
+                     sec: prevProps.sec + 1
+                    
+                }))
+          }, 1000);
+        } else if (setIsActive(!isActive)) {
+          clearInterval(interval);
+          setValues({...values,finished:true});
+        }
+      }, []);
+      
     const{text,userInput,symbols,sec,started,finished}=values;
 
     const onRestart=event=>{
@@ -38,26 +58,27 @@ const Typo=()=> {
     const setTime=()=> {
         if (!started) {
             setValues({ ...values, started: true })
-            interval.current = setInterval(() => {
-                setValues(prevProps => (
-                {
-                    ...prevProps,
-                     sec: prevProps.sec + 1
+            // interval.current = setInterval(() => {
+            //     setValues(prevProps => (
+            //     {
+            //         ...prevProps,
+            //          sec: prevProps.sec + 1
                     
-                }))
-            }, 1000)
+            //     }))
+            // }, 1000)
+            setIsActive(!isActive);
         }
     }
 
-    const onFinish=userInput=>{
-        
-        if(userInput==text)
-        {
-            clearInterval(interval.current)
-            setValues({...values,finished:true});
-            console.log("finished")
+    const onFinish=UI=>{
+        if(UI===text)
+        {  
+            // clearInterval(interval.current);
+            // console.log("clear interval executed successfully");
+            setIsActive(!isActive);
+            // setValues({...values,finished:true})
         }
-
+        return
     }
 
     const countSymbols=userinput=>{
@@ -74,11 +95,12 @@ const Typo=()=> {
                     <div className="relative mb-4">
                         <textarea className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-2 px-8 resize-none leading-6 transition-colors duration-200 ease-in-out"
                         placeholder="Start Typing..."
+                        readOnly={finished}
                         value={userInput}
                         onChange={userInputChange}
-                        readOnly={finished}>
+                        >
                         </textarea>
-                        <Speed sec={sec} symbols={symbols} finished={finished}/>
+                        <Speed sec={sec} symbols={symbols}/>
                         <div class="float-right">
                             <button onClick={onRestart} class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-md">Restart</button>
                         </div>
